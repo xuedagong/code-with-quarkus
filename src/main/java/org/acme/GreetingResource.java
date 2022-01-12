272c1e2b50b1c426465c38b64faeaf3b894aafc9
xuedagong <xuedagong@qq.com>
2022-01-12 10:37:14 +0800

    first commit

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,23 @@
+package org.acme;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+
+    @GET
+    @Path("/a")
+    public Object test(){
+        return Arrays.asList("nihao","中文 ");
+    }
+}
